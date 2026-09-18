117fe5d42f4275056f136aba302fde8d07928c82
Tayler <tayleraugusto17@gmail.com>
2026-09-18 15:20:20 -0300

    Ajusta tamanho da fonte dos botões para 10px

--- app.py
+++ app.py
@@ -687,9 +687,9 @@ div[data-testid="stExpander"] summary p {
     /* BLOCOS COM EXATAMENTE 3 COLUNAS (Ex: Navegação Anterior/Próximo) */
     div[data-testid="stHorizontalBlock"] > div[data-testid="column"]:first-child:nth-last-child(3),
     div[data-testid="stHorizontalBlock"] > div[data-testid="column"]:first-child:nth-last-child(3) ~ div:nth-child(3) {
-        flex: 0 0 30px !important;
-        max-width: 30px !important;
-        min-width: 30px !important;
+        flex: 0 0 10px !important;
+        max-width: 10px !important;
+        min-width: 10px !important;
     }
     
     div[data-testid="stHorizontalBlock"] > div[data-testid="column"]:first-child:nth-last-child(3) ~ div:nth-child(2) {
@@ -699,10 +699,10 @@ div[data-testid="stExpander"] summary p {
     /* ESTILO ESPECÍFICO PARA OS BOTÕES DAS LATERAIS (Anterior/Próximo) */
     div[data-testid="stHorizontalBlock"] > div[data-testid="column"]:first-child:nth-last-child(3) button,
     div[data-testid="stHorizontalBlock"] > div[data-testid="column"]:first-child:nth-last-child(3) ~ div:nth-child(3) button {
-        width: 30px !important;
-        height: 30px !important;
+        width: 10px !important;
+        height: 10px !important;
         padding: 0 !important;
-        font-size: 15px !important;
+        font-size: 6px !important;
         border-radius: 10px !important;
         margin: 0 auto !important;
         display: flex !important;
